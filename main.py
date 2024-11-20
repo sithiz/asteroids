394110eb6c3ded8b54f3fdb0a6b716025bb857d6
sithiz <Joshklaas1@gmail.com>
2024-11-20 10:49:09 -0600

    added groups and intergrated groups

--- main.py
+++ main.py
@@ -12,16 +12,25 @@ def main() :
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-    player = Player(x,y)
-
+    
     clock = pygame.time.Clock()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable,drawable)
+    player = Player(x,y)
 
     dt = 0
 
     while True :
+        for item in updatable :
+            item.update(dt)
         screen.fill("black")
-        player.draw(screen)
+        for item in drawable:
+            item.draw(screen)
+
+            
+
         
         pygame.display.flip()
         
