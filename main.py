6a1d1b5e51a81a2ddc5193c2d276e37d743c37d8
sithiz <Joshklaas1@gmail.com>
2024-11-19 18:56:17 -0600

    added draw box and initalized game

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 import pygame
-
+from constants import *
 def main() :
+    pygame.init()
     print("Starting asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True :
+        screen.fill("black")
+        pygame.display.flip()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+    
+   
 
 
 
