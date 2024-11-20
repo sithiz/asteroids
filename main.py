c7f5b90f63668e103563740ae7fe7a861df14fd5
sithiz <Joshklaas1@gmail.com>
2024-11-19 20:52:13 -0600

    Added player class and drew player to screen

--- main.py
+++ main.py
@@ -1,17 +1,40 @@
 import pygame
 from constants import *
+from player import Player
+
+x = SCREEN_WIDTH / 2
+y = SCREEN_HEIGHT / 2
+
+
+
 def main() :
     pygame.init()
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    player = Player(x,y)
+
+    clock = pygame.time.Clock()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    dt = 0
+
     while True :
         screen.fill("black")
+        player.draw(screen)
+        
         pygame.display.flip()
+        
+        dt = clock.tick(60) /1000   
+            
+        
+
+
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+     
     
    
 
